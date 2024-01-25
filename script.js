3d593a603560b2623c19a21246acc472743d11d2
abhishek singh <abhishekkumarsingh276642@gmail.com>
2024-01-25 14:36:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//Arrays Collection Of Items
+// let marks = [96,75,48,83,66];
+
+// console.log(marks);
+// marks[2]=49;
+// console.log(marks)
+// console.log(marks[0]);
+
+// let heroes = ["Captain America", "ironman", "thor", "hulk", "spiderman", "antman"];
+// for(let i =0; i<heroes.length; i++){
+//     console.log(heroes[i]);
+// }
+
+
+//for of
+
+// for(let hero of heroes){
+//     console.log(hero);
+// }
+
+// let cities = ["noida", "delhi", "gorakhpur", "pune", "dehradun", "nanital"];
+// for(let city of cities){
+//     console.log(city.toUpperCase());
+// }
